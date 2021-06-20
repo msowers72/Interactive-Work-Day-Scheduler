91a1c871eb62f6fe82e921652662a77a672d424c
Mike Sowers <msowers72@yahoo.com>
2021-06-20 15:40:30 -0400

    add time div to my html and function to java script page

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+/*GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist*/
+
+
+// DOM elements
+var timeDisplayEl = $('#time-display');
+
+// Display the time
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  setInterval(displayTime, 1000);
